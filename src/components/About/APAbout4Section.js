ce49b2898ba2eb26e34024e649c97b615e252c7b
Luis Enrique Concuá <hola@bambu.team>
2021-05-13 09:48:22 -0600

    Text + Data

--- src/components/About/APAbout4Section.js
+++ src/components/About/APAbout4Section.js
@@ -8,14 +8,9 @@ class ApAbout4Section extends Component {
 					<div className="row">
 						<div className="col-xl-6 col-lg-6">
 							<div className="about-info mb-30">
-								<h1>Welcome To <br/> La Herencia</h1>
-								<span>with love & dedication</span>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-									incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud
-									exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-									irure dolor in reprehenderit in voluptate velit esse fugiat nulla pariatur.
-									Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt
-									mollit anim id est laborum. Sed ut perspiciatis unde omnis iste.</p>
+								<h1>Welcome To <br/> La Herencia Spices</h1>
+								{/* <span>with love & dedication</span> */}
+								<p>Since establishment in 2012, La Herencia Spices has thrived on customer service and satisfaction, developing their product according to its needs. Located in Union City, New Jersey, La Herencia family has diversified their knowledge and line of products. Creating a new line of teas, Claudio’s Herbs, and a new line of snacks, Bersy’s Snacks. After the raw good importation, the company handpicks the best product to package and goes through careful inspection to ensure the best product. To guarantee customer satisfaction, the company keeps up to date with service as they have their section clean, organized, and full of product. La Herencia knows spices from how to grow them, to best storage options, and how to showcase the flavor to connect with many different customers and expand their line of <strong>Spices, Teas, and Snacks!</strong></p>
 							</div>
 						</div>
 						<div className="col-xl-6 col-lg-6">
