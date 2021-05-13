c6dafd8afbbffc290ec3753463a3323824c53f8b
Oscar1720 <oscar1720antonio@gmail.com>
2021-05-13 11:59:50 -0600

    EmailJS

--- src/components/Contact/CPContactFormSection.js
+++ src/components/Contact/CPContactFormSection.js
@@ -1,12 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import emailjs from 'emailjs-com';
+import swal from 'sweetalert';
 
 class CPContactFormSection extends Component {
 	render() {
+		function sendEmail(e) {
+			e.preventDefault();
+
+			emailjs.sendForm('service_n5uhvb7', 'template_j50b88q', e.target, 'user_KxHVvVglxkqJzNGPlhlem')
+				.then((result) => {
+					console.log(result.text);
+				}, (error) => {
+					console.log(error.text);
+				});
+			e.target.reset();
+		}
+		const mostrarAlert = () => {
+
+			swal({
+				title: "LA HERENCIA SPICES ",
+				text: "They are communicating with you soon, we are La Herencia Spices ",
+				icon: "success",
+				timer: 4000
+			});
+		}
 		return (
 			<div className="contact-area">
 				<div className="container">
 					<div className="contact-bg pt-90 pb-70"
-					     style={{backgroundImage: `url(${'assets/img/bg/bg18.jpg'})`}}>
+						style={{ backgroundImage: `url(${'assets/img/bg/bg18.jpg'})` }}>
 						<div className="row">
 							<div className="col-xl-12">
 								<div className="contact-title text-center mb-35">
@@ -17,24 +39,24 @@ class CPContactFormSection extends Component {
 						<div className="row">
 							<div className="col-xl-12">
 								<div className="contact-wrapper mb-30">
-									<form action="#" id="contact-us-form">
+									<form onSubmit={sendEmail} action="#" id="contact-us-form">
 										<div className="row">
 											<div className="col-xl-6 col-lg-6">
-												<input name="name" placeholder="Name :" type="text"/>
+												<input name="name" placeholder="Name :" type="text" />
 											</div>
 											<div className="col-xl-6 col-lg-6">
-												<input name="email" placeholder="Email :" type="email"/>
+												<input name="email" placeholder="Email :" type="email" />
 											</div>
 											<div className="col-xl-12 col-lg-12">
-												<input name="website" placeholder="Website :" type="text"/>
+												<input name="website" placeholder="Website :" type="text" />
 											</div>
 											<div className="col-xl-12 col-lg-12">
 												<textarea name="message" cols="30" rows="10"
-												          placeholder="Write your comments :"/>
+													placeholder="Write your comments :" />
 											</div>
 											<div className="col-xl-12 col-lg-12">
 												<div className="contact-button text-center">
-													<button className="btn" type="submit">Send Message</button>
+													<button onClick={() => mostrarAlert()} className="btn" type="submit">Send Message</button>
 												</div>
 											</div>
 										</div>
