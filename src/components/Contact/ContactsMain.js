c0d21d10062f18343b7424b358c6c8faed176483
Luis Enrique Concuá <hola@bambu.team>
2021-05-14 17:48:28 -0600

    MAP ALAV

--- src/components/Contact/ContactsMain.js
+++ src/components/Contact/ContactsMain.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import CPContactInfoSection from "./CPContactInfoSection";
 import CPContactFormSection from "./CPContactFormSection";
-import MapGoogle from './mapgoogle';
+
 
 class ContactsMain extends Component {
 	render() {
@@ -17,7 +17,7 @@ class ContactsMain extends Component {
 
 				{/* cta-start */}
 				{/* <CPMapSection/> */}
-				<MapGoogle />
+				
 				{/* cta-end */}
 			</main>
 		);
