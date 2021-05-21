3f37e1b7fb138b686ba97bb582bdbd78ad2cd97c
Oscar1720 <oscar1720antonio@gmail.com>
2021-05-21 14:48:32 -0600

    Cambios HOME

--- src/components/Elements/Tab/ProductSection.js
+++ src/components/Elements/Tab/ProductSection.js
@@ -36,7 +36,7 @@ class ProductSection extends Component {
 					<Tabs className="row" selectedTabClassName="active">
 						<div className="col-xl-12">
 							<TabList className="nav product-tab justify-content-center mb-75">
-								
+
 								<Tab className="nav-item">
 									<div className="product-tab-content text-center">
 										<div className="product-tab-img">
@@ -69,21 +69,22 @@ class ProductSection extends Component {
 							<TabPanel>
 								<div className="row">
 									<div className="col-xl-3 col-lg-3 col-md-6">
-										<div className="product-wrapper text-center mb-30">
+										<div className="text-center mb-30">
+											{/* <div className="product-wrapper text-center mb-30"> */}
 											<div className="product-img">
-												<Link href="/shop-details" as="/shop-details" >
+												<Link href= "#" as= "#" >
 													<a><img src={require("../../../../public/assets/img/product/cola de caballo.png")} alt="product" /></a>
 												</Link>
 
-												<div className="product-action">
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'shopping-cart']} /></i></a>
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'heart']} /></i></a>
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'search']} /></i></a>
-												</div>
+												{/* <div className="product-action">
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'shopping-cart']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'heart']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'search']} /></i></a>
+												</div> */}
 											</div>
 											<div className="product-text">
 												<h4>
-													<Link href="/shop-details" as="/shop-details" >
+													<Link href= "#" as= "#" >
 														<a>Horsetail Grass</a>
 													</Link>
 												</h4>
@@ -101,20 +102,21 @@ class ProductSection extends Component {
 										</div>
 									</div>
 									<div className="col-xl-3 col-lg-3 col-md-6">
-										<div className="product-wrapper text-center mb-30">
+										<div className="text-center mb-30">
+											{/* <div className="product-wrapper text-center mb-30"> */}
 											<div className="product-img">
-												<Link href="/shop-details" as="/shop-details" >
+												<Link href= "#" as= "#" >
 													<a><img src={require("../../../../public/assets/img/product/hoja de mango.png")} alt="image" /></a>
 												</Link>
 												<div className="product-action">
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'shopping-cart']} /></i></a>
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'heart']} /></i></a>
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'search']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'shopping-cart']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'heart']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'search']} /></i></a>
 												</div>
 											</div>
 											<div className="product-text">
 												<h4>
-													<Link href="/shop-details" as="/shop-details" >
+													<Link href= "#" as= "#" >
 														<a>Mango Leaf</a>
 													</Link>
 												</h4>
@@ -132,20 +134,21 @@ class ProductSection extends Component {
 										</div>
 									</div>
 									<div className="col-xl-3 col-lg-3 col-md-6">
-										<div className="product-wrapper text-center mb-30">
+										<div className="text-center mb-30">
+											{/* <div className="product-wrapper text-center mb-30"> */}
 											<div className="product-img">
-												<Link href="/shop-details" as="/shop-details" >
+												<Link href= "#" as= "#" >
 													<a><img src={require("../../../../public/assets/img/product/tilo.png")} alt="image" /></a>
 												</Link>
 												<div className="product-action">
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'shopping-cart']} /></i></a>
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'heart']} /></i></a>
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'search']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'shopping-cart']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'heart']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'search']} /></i></a>
 												</div>
 											</div>
 											<div className="product-text">
 												<h4>
-													<Link href="/shop-details" as="/shop-details" >
+													<Link href= "#" as= "#" >
 														<a>Linden Leaves</a>
 													</Link>
 												</h4>
@@ -163,20 +166,21 @@ class ProductSection extends Component {
 										</div>
 									</div>
 									<div className="col-xl-3 col-lg-3 col-md-6">
-										<div className="product-wrapper text-center mb-30">
+										<div className="text-center mb-30">
+											{/* <div className="product-wrapper text-center mb-30"> */}
 											<div className="product-img">
-												<Link href="/shop-details" as="/shop-details" >
+												<Link href= "#" as= "#" >
 													<a><img src={require("../../../../public/assets/img/product/teestres.png")} alt="image" /></a>
 												</Link>
 												<div className="product-action">
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'shopping-cart']} /></i></a>
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'heart']} /></i></a>
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'search']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'shopping-cart']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'heart']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'search']} /></i></a>
 												</div>
 											</div>
 											<div className="product-text">
 												<h4>
-													<Link href="/shop-details" as="/shop-details" >
+													<Link href= "#" as= "#" >
 														<a> Stress Tea</a>
 													</Link>
 												</h4>
@@ -198,20 +202,21 @@ class ProductSection extends Component {
 							<TabPanel>
 								<div className="row">
 									<div className="col-xl-3 col-lg-3 col-md-6">
-										<div className="product-wrapper text-center mb-30">
+										<div className="text-center mb-30">
+											{/* <div className="product-wrapper text-center mb-30"> */}
 											<div className="product-img">
-												<Link href="/shop-details" as="/shop-details" >
+												<Link href= "#" as= "#" >
 													<a><img src={require("../../../../public/assets/img/product/saltyfava1.png")} alt="image" /></a>
 												</Link>
 												<div className="product-action">
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'shopping-cart']} /></i></a>
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'heart']} /></i></a>
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'search']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'shopping-cart']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'heart']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'search']} /></i></a>
 												</div>
 											</div>
 											<div className="product-text">
 												<h4>
-													<Link href="/shop-details" as="/shop-details" >
+													<Link href= "#" as= "#" >
 														<a>Salty Fava Bean</a>
 													</Link>
 												</h4>
@@ -229,21 +234,22 @@ class ProductSection extends Component {
 										</div>
 									</div>
 									<div className="col-xl-3 col-lg-3 col-md-6">
-										<div className="product-wrapper text-center mb-30">
+										<div className="text-center mb-30">
+											{/* <div className="product-wrapper text-center mb-30"> */}
 											<div className="product-img">
-												<Link href="/shop-details" as="/shop-details" >
+												<Link href= "#" as= "#" >
 													<a><img src={require("../../../../public/assets/img/product/saltyfava.png")} alt="product" /></a>
 												</Link>
 
 												<div className="product-action">
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'shopping-cart']} /></i></a>
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'heart']} /></i></a>
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'search']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'shopping-cart']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'heart']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'search']} /></i></a>
 												</div>
 											</div>
 											<div className="product-text">
 												<h4>
-													<Link href="/shop-details" as="/shop-details" >
+													<Link href= "#" as= "#" >
 														<a>Spicy Fava Bean</a>
 													</Link>
 												</h4>
@@ -261,20 +267,21 @@ class ProductSection extends Component {
 										</div>
 									</div>
 									<div className="col-xl-3 col-lg-3 col-md-6">
-										<div className="product-wrapper text-center mb-30">
+										<div className="text-center mb-30">
+											{/* <div className="product-wrapper text-center mb-30"> */}
 											<div className="product-img">
-												<Link href="/shop-details" as="/shop-details" >
+												<Link href= "#" as= "#" >
 													<a><img src={require("../../../../public/assets/img/product/crisppumpkin.png")} alt="image" /></a>
 												</Link>
 												<div className="product-action">
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'shopping-cart']} /></i></a>
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'heart']} /></i></a>
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'search']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'shopping-cart']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'heart']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'search']} /></i></a>
 												</div>
 											</div>
 											<div className="product-text">
 												<h4>
-													<Link href="/shop-details" as="/shop-details" >
+													<Link href= "#" as= "#" >
 														<a>Crisp Pumpkin</a>
 													</Link>
 												</h4>
@@ -292,20 +299,21 @@ class ProductSection extends Component {
 										</div>
 									</div>
 									<div className="col-xl-3 col-lg-3 col-md-6">
-										<div className="product-wrapper text-center mb-30">
+										<div className="text-center mb-30">
+											{/* <div className="product-wrapper text-center mb-30"> */}
 											<div className="product-img">
-												<Link href="/shop-details" as="/shop-details" >
+												<Link href= "#" as= "#" >
 													<a><img src={require("../../../../public/assets/img/product/prunes.png")} alt="image" /></a>
 												</Link>
 												<div className="product-action">
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'shopping-cart']} /></i></a>
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'heart']} /></i></a>
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'search']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'shopping-cart']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'heart']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'search']} /></i></a>
 												</div>
 											</div>
 											<div className="product-text">
 												<h4>
-													<Link href="/shop-details" as="/shop-details" >
+													<Link href= "#" as= "#" >
 														<a>Prunes</a>
 													</Link>
 												</h4>
@@ -327,20 +335,21 @@ class ProductSection extends Component {
 							<TabPanel>
 								<div className="row">
 									<div className="col-xl-3 col-lg-3 col-md-6">
-										<div className="product-wrapper text-center mb-30">
+										<div className="text-center mb-30">
+											{/* <div className="product-wrapper text-center mb-30"> */}
 											<div className="product-img">
-												<Link href="/shop-details" as="/shop-details" >
+												<Link href= "#" as= "#" >
 													<a><img src={require("../../../../public/assets/img/product/Anise.png")} alt="image" /></a>
 												</Link>
 												<div className="product-action">
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'shopping-cart']} /></i></a>
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'heart']} /></i></a>
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'search']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'shopping-cart']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'heart']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'search']} /></i></a>
 												</div>
 											</div>
 											<div className="product-text">
 												<h4>
-													<Link href="/shop-details" as="/shop-details" >
+													<Link href= "#" as= "#" >
 														<a>Anise Seed</a>
 													</Link>
 												</h4>
@@ -358,20 +367,21 @@ class ProductSection extends Component {
 										</div>
 									</div>
 									<div className="col-xl-3 col-lg-3 col-md-6">
-										<div className="product-wrapper text-center mb-30">
+										<div className="text-center mb-30">
+											{/* <div className="product-wrapper text-center mb-30"> */}
 											<div className="product-img">
-												<Link href="/shop-details" as="/shop-details" >
+												<Link href= "#" as= "#" >
 													<a><img src={require("../../../../public/assets/img/product/Spices.png")} alt="image" /></a>
 												</Link>
 												<div className="product-action">
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'shopping-cart']} /></i></a>
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'heart']} /></i></a>
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'search']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'shopping-cart']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'heart']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'search']} /></i></a>
 												</div>
 											</div>
 											<div className="product-text">
 												<h4>
-													<Link href="/shop-details" as="/shop-details" >
+													<Link href= "#" as= "#" >
 														<a>Spices</a>
 													</Link>
 												</h4>
@@ -389,21 +399,22 @@ class ProductSection extends Component {
 										</div>
 									</div>
 									<div className="col-xl-3 col-lg-3 col-md-6">
-										<div className="product-wrapper text-center mb-30">
+										<div className="text-center mb-30">
+											{/* <div className="product-wrapper text-center mb-30"> */}
 											<div className="product-img">
-												<Link href="/shop-details" as="/shop-details" >
+												<Link href= "#" as= "#" >
 													<a><img src={require("../../../../public/assets/img/product/chia.png")} alt="product" /></a>
 												</Link>
 
 												<div className="product-action">
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'shopping-cart']} /></i></a>
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'heart']} /></i></a>
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'search']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'shopping-cart']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'heart']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'search']} /></i></a>
 												</div>
 											</div>
 											<div className="product-text">
 												<h4>
-													<Link href="/shop-details" as="/shop-details" >
+													<Link href= "#" as= "#" >
 														<a>Chia Seed</a>
 													</Link>
 												</h4>
@@ -421,20 +432,21 @@ class ProductSection extends Component {
 										</div>
 									</div>
 									<div className="col-xl-3 col-lg-3 col-md-6">
-										<div className="product-wrapper text-center mb-30">
+										<div className="text-center mb-30">
+											{/* <div className="product-wrapper text-center mb-30"> */}
 											<div className="product-img">
-												<Link href="/shop-details" as="/shop-details" >
+												<Link href= "#" as= "#" >
 													<a><img src={require("../../../../public/assets/img/product/Poultry.png")} alt="image" /></a>
 												</Link>
 												<div className="product-action">
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'shopping-cart']} /></i></a>
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'heart']} /></i></a>
-													<a href="#"><i><FontAwesomeIcon icon={['fas', 'search']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'shopping-cart']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'heart']} /></i></a>
+													<a href= "#"><i><FontAwesomeIcon icon={['fas', 'search']} /></i></a>
 												</div>
 											</div>
 											<div className="product-text">
 												<h4>
-													<Link href="/shop-details" as="/shop-details" >
+													<Link href= "#" as= "#" >
 														<a>Poultry Seasoning</a>
 													</Link>
 												</h4>
